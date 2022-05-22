bf78b2c2550c0b79a8cc7612ee1e58ccd555fb3e
danfild <danfild_kazmin@mail.ru>
2022-05-23 02:16:24 +0500

    todolist

--- client/src/components/TodoList/Index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const TodoList = () => {
-    return (
-        <div>
-            <h2>{'TodoList'}</h2>
-        </div>
-    )
-}
--- client/src/pages/CreatePage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const CreatePage = () => {
-    return (
-        <div>
-            <h2>{'CreatePage'}</h2>
-        </div>
-    )
-}
